4dc08184897d341d5e20ce47a9280944f704dae8
Elior <38843046+Eliorkalfon@users.noreply.github.com>
2024-05-07 15:46:35 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -22,14 +22,14 @@ def predict_test(data, models, n_components_list, d_list, batch_size, device='cu
             combined_outputs.append(transformed_data)
 
             # Stack the combined outputs
-            combined_outputs = torch.stack(combined_outputs, dim=0)
-            sample_submission = pd.read_csv(
-                f"sample_submission.csv")
-            sample_columns = sample_submission.columns
-            sample_columns = sample_columns[1:]
-            submission_df = pd.DataFrame(combined_outputs.cpu().detach().numpy()[0], columns=sample_columns)
-            submission_df.insert(0, 'id', range(255))
-            submission_df.to_csv(f"result_{n_components}_{d_model}.csv", index=False)
+        combined_outputs = torch.stack(combined_outputs, dim=0)
+        sample_submission = pd.read_csv(
+            f"sample_submission.csv")
+        sample_columns = sample_submission.columns
+        sample_columns = sample_columns[1:]
+        submission_df = pd.DataFrame(combined_outputs.cpu().detach().numpy()[0], columns=sample_columns)
+        submission_df.insert(0, 'id', range(255))
+        submission_df.to_csv(f"result_{n_components}_{d_model}.csv", index=False)
 
     return
 
